be5bc1518ae1cade3d0418be60a5061900ba218a
DoctorFuchs <61468448+DoctorFuchs@users.noreply.github.com>
2021-08-20 12:20:49 +0200

    style

--- config.go
+++ config.go
@@ -1,40 +1,47 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"os"
-	"encoding/json"
 )
 
-type cfg struct{
-	SaltSize int
-    UserIDLength int
-    TodoIDLength int
-    DbUri string
-    DbName string
-    UserDataCollectionName string
-    TodoCollectionName string
-	MaxTodoAmt int
+type cfg struct {
+	SaltSize               int
+	UserIDLength           int
+	TodoIDLength           int
+	DbUri                  string
+	DbName                 string
+	UserDataCollectionName string
+	TodoCollectionName     string
+	MaxTodoAmt             int
 }
 
-var config cfg;
+var config cfg
 
-func readConfig(){
-	if ! readConfigFromFile("./config/config.json") {
+func readConfig() {
+	if !readConfigFromFile("./config/config.json") {
 		fmt.Println("Could not read config, attempting to read default config file...")
-		if readConfigFromFile("./config/config.default.json"){
-			fmt.Println("Sucess!")
-		}else{fmt.Println("Could not read config, quitting..."); os.Exit(1)}
+		if readConfigFromFile("./config/config.default.json") {
+			fmt.Println("Success!")
+		} else {
+			fmt.Println("Could not read config, quitting...")
+			os.Exit(1)
+		}
 	}
-	fmt.Println("Sucessfully read config file")
+	fmt.Println("Successfully read config file")
 }
 
-func readConfigFromFile(filename string) bool{
+func readConfigFromFile(filename string) bool {
 
 	file, err := os.Open(filename)
-	if err != nil {  return false }
+	if err != nil {
+		return false
+	}
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&config)
-	if err != nil {  return false }
+	if err != nil {
+		return false
+	}
 	return true
 }
